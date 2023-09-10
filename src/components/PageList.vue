2d7e24c687d52ec579e6525fddbb0d01b4258e88
Saravanan Palanisamy <126825088+Dhibicoders@users.noreply.github.com>
2023-09-10 09:13:26 +0530

    Create PageList.vue

---
+++ src/components/PageList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="pages">
+    <div v-for="page in pages" :key="page.id" :class="{ page: 1, selected: isSelected(page) }" @click="selectPage(page.id)">
+      {{ page.name || page.id }} <span v-if="!isSelected(page)" @click="removePage(page.id)" class="page-close">&Cross;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pages: Array,
+  },
+  methods: {
+    isSelected(page) {
+      return pm.getSelected().id === page.id;
+    },
+    selectPage(pageId) {
+      return pm.select(pageId);
+    },
+    removePage(pageId) {
+      return pm.remove(pageId);
+    },
+  },
+};
+</script>
